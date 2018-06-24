2e9cc027b14116be723e3d37081561709d4338de
Alexandre Moraes <alcmoraes89@gmail.com>
2018-06-24 19:02:44 -0300

    First commit :)

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	ENDPOINT := os.Args[1]
+
+	c := colly.NewCollector()
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	})
+
+	// Find and visit all links
+	c.OnHTML("a.project[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		repositoryDownload := ENDPOINT + link + "/repository/archive.zip?ref=master"
+		err := DownloadFile(strings.Replace(link, "/", "-", -1)[1:]+".zip", repositoryDownload)
+		if err != nil {
+			panic(err)
+		}
+
+	})
+
+	c.OnHTML("li.next a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit(ENDPOINT + "/explore/projects")
+}
+
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create("repos/" + filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
